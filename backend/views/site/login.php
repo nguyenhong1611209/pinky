499cc0b2ac3438a94c161db24972075cc34954dc
nguyenhong1611209 <nguyenhong1611209@gmail.com>
2021-05-11 09:26:58 +0700

    Integrate hcmgis-ecommerce in project

---
+++ backend/views/site/login.php
@@ -0,0 +1,48 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap4\ActiveForm */
+/* @var $model LoginForm */
+
+use common\models\LoginForm;
+use yii\helpers\Html;
+use yii\bootstrap4\ActiveForm;
+use yii\helpers\Url;
+
+$this->title = 'Login';
+?>
+
+<div class="row">
+    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+    <div class="col-lg-6">
+        <div class="p-5">
+            <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+            </div>
+            <?php $form = ActiveForm::begin(['id' => 'login-form', 'options'=>['class'=>'user']]); ?>
+            <form class="user">
+                    <?= $form->field($model, 'username',[
+                            'inputOptions'=>[
+                                    'class'=>'form-control form-control-user',
+                                'placeholder'=>'Nhap email'
+                            ]
+                    ])->textInput(['autofocus' => true]) ?>
+                    <?= $form->field($model, 'password',[
+                            'inputOptions'=>[
+                                'class'=>'form-control form-control-user',
+                                'placeholder'=>'Nhap mk'
+                            ]
+                    ])->passwordInput() ?>
+                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                    <?= Html::submitButton('Login',
+                        ['class' => 'btn btn-primary btn-user btn-block',
+                            'name' => 'login-button']) ?>
+            </form>
+            <?php ActiveForm::end(); ?>
+            <hr>
+            <div class="text-center">
+                <a class="small" href="<?php echo Url::to(['/site/forgot-password'])?>">Forgot Password?</a>
+            </div>
+        </div>
+    </div>
+</div>
